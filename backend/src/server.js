63739203b72622035ee34428d29ee43106289f42
Tanish_Nagpal <tanish1739.becse24@chitkara.edu.in>
2026-02-09 11:00:36 +0530

    Fixed frontend path in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,21 +15,15 @@ app.get("/books", (req, res) => {
   res.json({ message: "this is the books endpoint" });
 });
 
-// SERVE FRONTEND (ALWAYS)
-const clientDistPath = path.join(process.cwd(), "frontend", "dist");
+//Fixed frontend path
+const clientDistPath = path.resolve("frontend", "dist");
 
 app.use(express.static(clientDistPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(clientDistPath, "index.html"));
+  res.sendFile(path.resolve(clientDistPath, "index.html"));
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log("Server is running on port:", process.env.PORT || 8080);
+  console.log("Server is running on port:", process.env.PORT);
 });
-
-
-
-
-
-
