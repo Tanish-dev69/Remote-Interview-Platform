66f61ab67bf915b562325da56e54041e95e8e87f
Tanish_Nagpal <tanish1739.becse24@chitkara.edu.in>
2026-02-19 23:01:54 +0530

    frontend tool setup

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
+import React from 'react'
+
+import  toast  from "react-hot-toast";
+
+function HomePage() {
+ 
+  return (
+    <div>
+      <button className="btn btn-secondary" onClick={() => toast.error("Logged in successfully!")}>Clickme</button>
+  
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button>Login</button>
+        </SignInButton>
+      </SignedOut>
+  
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+  
+      <UserButton />
+    </div>
+  );
+}
+export default HomePage;
